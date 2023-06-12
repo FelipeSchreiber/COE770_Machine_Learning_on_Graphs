ec8171e3dfaae425c60d7894f6d35508ccfa08ab
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-12 01:04:59 -0300

    Changed softmax to sigmoid

--- model/ADCRNN.py
+++ model/ADCRNN.py
@@ -219,7 +219,7 @@ class ADCRNN(torch.nn.Module):
         adj_mat = adj_mat.reshape(adj_mat.size(1), adj_mat.size(2))
         matrix_sim = torch.mm(X, X.transpose(0, 1))
         print(matrix_sim.shape)
-        self.residual_matrix = NN.Softmax(NN.PReLU(matrix_sim))
+        self.residual_matrix = NN.Sigmoid(NN.PReLU(matrix_sim))
         
         adj_mat += self.residual_matrix
         edge_index_, edge_weight_ = dense_to_sparse(adj_mat)
