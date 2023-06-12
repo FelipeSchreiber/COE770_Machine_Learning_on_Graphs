2ebd2e31fa577367acff038a9f12bf9c394d736d
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-11 23:56:27 -0300

    ""

--- model/covid_model.py
+++ model/covid_model.py
@@ -2,9 +2,39 @@ import torch
 import torch.nn
 import torch.nn.functional as F
 from torch_geometric_temporal.nn.recurrent import DCRNN
+from .ADCRNN import ADCRNN
 import gdown
 import os
 
+class RecurrentGCN(torch.nn.Module):
+    def __init__(self, num_features=35,num_filters=3):
+        super(RecurrentGCN, self).__init__()
+        self.recurrent = DCRNN(num_features, num_filters, 1)
+        self.linear = torch.nn.Linear(num_filters, 1)
+
+    def forward(self, x, edge_index, edge_weight):
+        #,A_h,A_z,A_r 
+        h,A_h,A_z,A_r = self.recurrent(x, edge_index, edge_weight)
+        h = F.prelu(h)
+        h = self.linear(h)
+        return h,A_h,A_z,A_r 
+    
+def get_model(get_whole_model=True,num_features=35,num_filters=3):
+    if get_whole_model:
+        url = "https://drive.google.com/uc?export=download&id=1XYICNGVXOEqazVJmfEfxlLiy7QoNeQzs"
+        output = "covid_model"
+        gdown.download(url, output)
+        cwd = os.getcwd()+"/"
+        return torch.load(cwd+output)
+    else:
+        url = "https://drive.google.com/uc?export=download&id=1skGodpbZZhuDyvxJbicnIe8ed9d0w5iN"
+        output = "covid_model_weights"
+        gdown.download(url, output)
+        cwd = os.getcwd()+"/"
+        covid_model = RecurrentGCN(num_features=num_features,num_filters=num_filters)
+        covid_model.load_state_dict(torch.load(cwd+output))
+        return covid_model
+"""    
 class GeomRNN(torch.nn.Module):
     
     def __init__(self, num_classes, input_size, hidden_size, num_layers):
@@ -34,30 +64,4 @@ class GeomRNN(torch.nn.Module):
         out = self.fc_2(out) # final output
         return out
 
-class RecurrentGCN(torch.nn.Module):
-    def __init__(self, num_features=35,num_filters=3):
-        super(RecurrentGCN, self).__init__()
-        self.recurrent = DCRNN(num_features, num_filters, 1)
-        self.linear = torch.nn.Linear(num_filters, 1)
-
-    def forward(self, x, edge_index, edge_weight):
-        h = self.recurrent(x, edge_index, edge_weight)
-        h = F.relu(h)
-        h = self.linear(h)
-        return h
-    
-def get_model(get_whole_model=True,num_features=35,num_filters=3):
-    if get_whole_model:
-        url = "https://drive.google.com/uc?export=download&id=1XYICNGVXOEqazVJmfEfxlLiy7QoNeQzs"
-        output = "covid_model"
-        gdown.download(url, output)
-        cwd = os.getcwd()+"/"
-        return torch.load(cwd+output)
-    else:
-        url = "https://drive.google.com/uc?export=download&id=1skGodpbZZhuDyvxJbicnIe8ed9d0w5iN"
-        output = "covid_model_weights"
-        gdown.download(url, output)
-        cwd = os.getcwd()+"/"
-        covid_model = RecurrentGCN(num_features=num_features,num_filters=num_filters)
-        covid_model.load_state_dict(torch.load(cwd+output))
-        return covid_model
+"""
