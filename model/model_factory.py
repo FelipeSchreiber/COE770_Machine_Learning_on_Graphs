9774cdb553273a80b8e77aa1415f3d31b12abf68
FelipeSchreiber <schreiber.felipe@gmail.com>
2023-06-17 00:07:51 -0300

    ""

--- model/model_factory.py
+++ model/model_factory.py
@@ -29,31 +29,19 @@ class MLP(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
-
-class A3TGCN_(torch.nn.Module):
-    def __init__(self, layer, out):
-        super(A3TGCN_, self).__init__()
-        self.recurrent = layer
-        self.linear = torch.nn.Linear(out, 1)
-
-    def forward(self, x, edge_index, edge_weight):
-        h = self.recurrent(x, edge_index, edge_weight)
-        h = F.relu(h)
-        h = self.linear(h)
-        return h
     
 class _A3TGCN_(torch.nn.Module):
-    def __init__(self, node_features, periods):
-        super( _A3TGCN_, self).__init__()
-        self.recurrent = A3TGCN_(node_features, 32, periods)
-        self.linear = torch.nn.Linear(32, 1)
+    def __init__(self, node_features, output_size=32, periods = 4):
+        super(_A3TGCN_, self).__init__()
+        self.recurrent = A3TGCN(node_features, output_size, periods)
+        self.linear = torch.nn.Linear(output_size, 1)
 
     def forward(self, x, edge_index, edge_weight):
         h = self.recurrent(x.view(x.shape[0], 1, x.shape[1]), edge_index, edge_weight)
         h = F.relu(h)
         h = self.linear(h)
         return h
-    
+        
 class _AGCNRN_(torch.nn.Module):
     def __init__(self, node_features = 35, output_size = 2, filter_size=2, num_nodes=5570):
         super(_AGCNRN_, self).__init__()
@@ -116,7 +104,7 @@ def make_models(layers:list,num_feats,output_size=32,filter_size=2):
     # models = []
     # for layer in layers:
     #     models.append(RecurrentGCN(layer,output_size))
-    models = [_A3TGCN_(node_features = num_feats, periods = 4),
+    models = [_A3TGCN_(node_features = num_feats, output_size = output_size),
               _AGCNRN_(node_features = num_feats, output_size = output_size, filter_size=filter_size),
               _DCRNN_(node_features= num_feats, output_size=output_size),
               _GConvGRU_(node_features= num_feats, output_size=output_size),
